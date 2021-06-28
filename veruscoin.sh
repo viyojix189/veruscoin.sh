6e86a64f6d5db483e94d16e619159a9407bd11b6
radenagungkertsaono <86518146+radenagungkertsaono@users.noreply.github.com>
2021-06-28 01:15:02 -0400

    Update and rename veruscoin to veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,10 @@
-#verus
+#!/bin/sh
+sudo apt update
+sudo apt isntall screen - y
 wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 10
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
